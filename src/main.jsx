bf659080cc0dabd2c20a07b8df3727124cd4c3e9
Crowsk1ck <crowskick4133@gmail.com>
2026-05-21 07:56:12 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './styles.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-<App />
-</BrowserRouter>
-)
+function App(){
+  return <h1 style={{color:'red'}}>GRIZZLY FIXED</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
