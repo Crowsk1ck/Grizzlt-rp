602a8b741addfa390b5dca7dff845c8404274624
Crowsk1ck <crowskick4133@gmail.com>
2026-05-21 13:17:12 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './styles.css'
 
-function App(){
-  return <h1 style={{color:'red'}}>GRIZZLY FIXED</h1>
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+<BrowserRouter>
+<App />
+</BrowserRouter>
+)
