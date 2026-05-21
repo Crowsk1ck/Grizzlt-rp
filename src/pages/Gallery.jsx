f82faf9a08329690096daa68909665ee59ac503b
Crowsk1ck <crowskick4133@gmail.com>
2026-05-21 16:12:40 +0200

    Update Gallery.jsx

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,1 +1,56 @@
-export default function Gallery(){return(<><h1 className='title'>Галерея</h1><div className='card'>📸 GTA RP MOMENTS</div></>)}
+export default function Gallery(){
+
+const images = [
+
+'/assets/gallery/1.jpg',
+'/assets/gallery/2.jpg',
+'/assets/gallery/3.jpg',
+'/assets/gallery/4.jpg',
+'/assets/gallery/5.jpg',
+'/assets/gallery/6.jpg'
+
+]
+
+return(
+
+<>
+
+<h1 className="title">
+GRIZZLY GALLERY
+</h1>
+
+<div className="galleryGrid">
+
+{
+images.map((img,index)=>(
+
+<div
+key={index}
+className="galleryCard"
+>
+
+<img
+src={img}
+className="galleryImage"
+/>
+
+<div className="galleryOverlay">
+
+<div className="galleryText">
+GRIZZLY FAMILY
+</div>
+
+</div>
+
+</div>
+
+))
+}
+
+</div>
+
+</>
+
+)
+
+}
