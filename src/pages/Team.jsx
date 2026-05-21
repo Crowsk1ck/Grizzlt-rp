4f1c275e835c2c1929f19619e3d217943bec529f
Crowsk1ck <crowskick4133@gmail.com>
2026-05-21 16:02:52 +0200

    Update Team.jsx

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,1 +1,97 @@
-export default function Team(){return(<><h1 className='title'>Команда</h1><div className='card'>👑 OWNER</div></>)}
+export default function Team(){
+
+const members = [
+
+{
+name:'CrowsKick',
+role:'OWNER',
+avatar:'https://i.imgur.com/8Km9tLL.png'
+},
+
+{
+name:'Maryana',
+role:'CO-OWNER',
+avatar:'https://i.imgur.com/8Km9tLL.png'
+},
+
+{
+name:'Zim',
+role:'LEADER',
+avatar:'https://i.imgur.com/8Km9tLL.png'
+},
+
+{
+name:'Neffiltrovanuyi',
+role:'WAR MANAGER',
+avatar:'https://i.imgur.com/8Km9tLL.png'
+},
+
+{
+name:'BlackWolf',
+role:'BUSINESS',
+avatar:'https://i.imgur.com/8Km9tLL.png'
+},
+
+{
+name:'Ghost',
+role:'EVENTS',
+avatar:'https://i.imgur.com/8Km9tLL.png'
+}
+
+]
+
+return(
+
+<>
+
+<h1 className="title">
+GRIZZLY TEAM
+</h1>
+
+<div className="teamGrid">
+
+{
+members.map((m,index)=>(
+
+<div
+key={index}
+className="teamCard"
+>
+
+<img
+src={m.avatar}
+className="teamAvatar"
+/>
+
+<div className="teamName">
+{m.name}
+</div>
+
+<div className="teamRole">
+{m.role}
+</div>
+
+<div className="teamButtons">
+
+<button className="teamBtn">
+PROFILE
+</button>
+
+<button className="teamBtn secondary">
+MESSAGE
+</button>
+
+</div>
+
+</div>
+
+))
+}
+
+</div>
+
+</>
+
+)
+
+}
