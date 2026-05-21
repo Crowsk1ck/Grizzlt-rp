c57310e6f8a2f028e4fcbc18ca65977e5762aa4d
Crowsk1ck <crowskick4133@gmail.com>
2026-05-21 06:48:47 +0200

    Add files via upload

---
+++ src/pages/Apply.jsx
@@ -0,0 +1,100 @@
+import { useState } from 'react'
+
+export default function Apply(){
+
+const [nick,setNick] = useState('')
+const [discord,setDiscord] = useState('')
+const [telegram,setTelegram] = useState('')
+const [reason,setReason] = useState('')
+const [loading,setLoading] = useState(false)
+const [success,setSuccess] = useState(false)
+
+const sendApplication = async () => {
+
+try{
+
+setLoading(true)
+
+await fetch(import.meta.env.VITE_DISCORD_WEBHOOK,{
+method:'POST',
+headers:{
+'Content-Type':'application/json'
+},
+body:JSON.stringify({
+content:
+`Nick: ${nick}
+Discord: ${discord}
+Telegram: ${telegram}
+
+Причина:
+${reason}`
+})
+})
+
+setSuccess(true)
+
+setNick('')
+setDiscord('')
+setTelegram('')
+setReason('')
+
+}catch(e){
+
+alert('Webhook error')
+
+}finally{
+setLoading(false)
+}
+
+}
+
+return(
+<>
+<h1 className="title">Заявка</h1>
+
+<div className="card">
+
+<input
+placeholder="Nick"
+value={nick}
+onChange={e=>setNick(e.target.value)}
+/>
+
+<input
+placeholder="Discord"
+value={discord}
+onChange={e=>setDiscord(e.target.value)}
+/>
+
+<input
+placeholder="Telegram"
+value={telegram}
+onChange={e=>setTelegram(e.target.value)}
+/>
+
+<textarea
+placeholder="Причина"
+value={reason}
+onChange={e=>setReason(e.target.value)}
+></textarea>
+
+<button
+className="btn"
+onClick={sendApplication}
+disabled={loading}
+>
+
+{loading ? 'Отправка...' : 'ОТПРАВИТЬ ЗАЯВКУ'}
+
+</button>
+
+{success && (
+<div className="infoBox">
+✅ Заявка отправлена в Discord
+</div>
+)}
+
+</div>
+</>
+)
+}
---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,11 @@
+export default function Gallery(){
+return(
+<>
+<h1 className="title">Галерея</h1>
+
+<div className="card">
+<div className="infoBox">📸 GTA RP MOMENTS</div>
+</div>
+</>
+)
+}
