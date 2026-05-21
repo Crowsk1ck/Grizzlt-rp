4cc29035d80cb4788cf83366c0cbf2a14491c7d8
Crowsk1ck <crowskick4133@gmail.com>
2026-05-21 16:59:32 +0200

    Update LoadingScreen.jsx

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,21 +1,65 @@
 import { useEffect, useState } from 'react'
 
-export default function LoadingScreen({onFinish}){
-const [progress,setProgress]=useState(0)
-useEffect(()=>{
-const i=setInterval(()=>{
-setProgress(p=>{
-if(p>=100){clearInterval(i);setTimeout(()=>onFinish(),500);return 100}
-return p+2
+export default function LoadingScreen({ onFinish }) {
+
+const [progress, setProgress] = useState(0)
+
+useEffect(() => {
+
+const interval = setInterval(() => {
+
+setProgress(prev => {
+
+if(prev >= 100){
+
+clearInterval(interval)
+
+setTimeout(() => {
+onFinish()
+},500)
+
+return 100
+}
+
+return prev + 1
+
 })
-},40)
-return ()=>clearInterval(i)
+
+},30)
+
+return () => clearInterval(interval)
+
 },[])
+
 return (
-<div className='loadingScreen'>
-<div className='loadingLogo'>GRIZZLY FAMILY</div>
-<div className='loadingBar'><div className='loadingFill' style={{width:`${progress}%`}}></div></div>
-<div className='loadingText'>LOADING {progress}%</div>
+
+<div className="loadingScreen">
+
+<div className="loadingCenter">
+
+<h1 className="loadingLogo">
+GRIZZLY FAMILY
+</h1>
+
+<div className="loadingBar">
+
+<div
+className="loadingFill"
+style={{
+width:`${progress}%`
+}}
+></div>
+
 </div>
+
+<div className="loadingPercent">
+LOADING {progress}%
+</div>
+
+</div>
+
+</div>
+
 )
+
 }
