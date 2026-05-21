3680b23c45ce2cddf9432b4f0276e396058088a5
Crowsk1ck <crowskick4133@gmail.com>
2026-05-21 06:28:17 +0200

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,103 @@
+export default function Home(){
+
+return(
+<>
+
+<h1 className="title">
+Інфо
+</h1>
+
+<div className="card">
+
+<h2 style={{color:'#ff004c'}}>
+⚡ GRIZZLY FAMILY
+</h2>
+
+<p style={{
+marginTop:'15px',
+lineHeight:'1.8',
+color:'#cfcfcf'
+}}>
+Преміальна GTA 5 RP сім’я
+з атмосферою Rockstar Games,
+неоновим стилем та активним
+RP ком’юніті.
+</p>
+
+</div>
+
+<div className="card">
+
+<h2 style={{color:'#ff004c'}}>
+📍 ІНФОРМАЦІЯ
+</h2>
+
+<div className="infoBox">
+👥 ACTIVE MEMBERS — 20+
+</div>
+
+<div className="infoBox">
+🏆 STATUS — OPEN RECRUITMENT
+</div>
+
+<div className="infoBox">
+📍 SERVER — NG UA
+</div>
+
+<div className="infoBox">
+🔥 FAMILY LEVEL — TOP RP
+</div>
+
+</div>
+
+<div className="card">
+
+<h2 style={{color:'#ff004c'}}>
+💀 ЧОМУ САМЕ МИ?
+</h2>
+
+<div className="infoBox">
+✔ Активне RP ком’юніті
+</div>
+
+<div className="infoBox">
+✔ Система контрактів
+</div>
+
+<div className="infoBox">
+✔ Discord інтеграція
+</div>
+
+<div className="infoBox">
+✔ Premium GTA Style
+</div>
+
+</div>
+
+<div className="card">
+
+<h2 style={{color:'#ff004c'}}>
+🚀 ROADMAP
+</h2>
+
+<div className="infoBox">
+✔ Recruitment System
+</div>
+
+<div className="infoBox">
+✔ Discord OAuth2
+</div>
+
+<div className="infoBox">
+✔ Application System
+</div>
+
+<div className="infoBox">
+🔜 Territory Wars
+</div>
+
+</div>
+
+</>
+)
+}
