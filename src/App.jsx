5a927c68ed3e0119da3eab044255de46d7bfdd18
Crowsk1ck <crowskick4133@gmail.com>
2026-05-22 22:30:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ return(
 
 <a
 className="loginBtn"
-href="https://discord.com/oauth2/authorize?client_id=1506029366008610856&response_type=code&redirect_uri=https%3A%2F%2Fwww.grizzly-family.online%2F&scope=identify"
+href="https://discord.com/oauth2/authorize?client_id=1506029366008610856&response_type=token&redirect_uri=https%3A%2F%2Fwww.grizzly-family.online%2F&scope=identify"
 >
 LOGIN WITH DISCORD
 </a>
