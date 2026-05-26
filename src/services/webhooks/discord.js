ec12ab5a0a9d0f7e134204d21f53d2eda9cf3498
Crowsk1ck <crowskick4133@gmail.com>
2026-05-26 14:16:13 +0200

    Add files via upload

---
+++ src/services/webhooks/discord.js
@@ -0,0 +1,3 @@
+export const applicationsWebhook = 'applications webhook connected'
+export const contractsWebhook = 'contracts webhook connected'
+export const adminWebhook = 'admin webhook connected'
