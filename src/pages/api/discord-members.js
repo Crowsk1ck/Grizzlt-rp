4654f00c3187732c8b9694109f69d0f6e717b2e5
Crowsk1ck <crowskick4133@gmail.com>
2026-05-27 18:41:53 +0200

    Update discord-members.js

--- src/pages/api/discord-members.js
+++ src/pages/api/discord-members.js
@@ -1,5 +1,8 @@
+
 export default function handler(req, res) {
 
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
   res.status(200).json({
     tokenExists: !!process.env.DISCORD_BOT_TOKEN,
     hello: true
