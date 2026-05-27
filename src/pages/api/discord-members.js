a62e9b072bf0b809bc037a45048fafeec51cba66
Crowsk1ck <crowskick4133@gmail.com>
2026-05-27 17:02:04 +0200

    Update discord-members.js

--- src/pages/api/discord-members.js
+++ src/pages/api/discord-members.js
@@ -1,7 +1,28 @@
-export default function handler(req,res){
+export default async function handler(req,res){
 
-  res.status(200).json({
-    members:148
-  })
+  try{
+
+    const response = await fetch(
+      'https://discord.com/api/v10/guilds/1388989912996380713?with_counts=true',
+      {
+        headers:{
+          Authorization:`Bot ${process.env.DISCORD_BOT_TOKEN}`
+        }
+      }
+    )
+
+    const data = await response.json()
+
+    res.status(200).json({
+      members:data.approximate_member_count || 0
+    })
+
+  }catch(error){
+
+    res.status(500).json({
+      members:0
+    })
+
+  }
 
 }
