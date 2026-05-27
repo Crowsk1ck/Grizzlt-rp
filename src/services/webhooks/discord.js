8df2d270905603174ae365cb034b6f56ff5d2079
Crowsk1ck <crowskick4133@gmail.com>
2026-05-27 05:37:54 +0200

    Update discord.js

--- src/services/webhooks/discord.js
+++ src/services/webhooks/discord.js
@@ -18,30 +18,3 @@ export async function sendWebhook(url,payload){
 export const contractsWebhook =
 'https://discord.com/api/webhooks/1506424883737788619/yATAISypU22ZWVvhRKMsSeSZT1l7bghWRvPSoLaERM8tdj1Wx70JXq4QU2DjYwiHC72F'
 import.meta.env.VITE_DISCORD_CONTRACTS_WEBHOOK
-export async function sendWebhook(
-  webhook,
-  data
-){
-
-  try{
-
-    await fetch(
-      webhook,
-      {
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json'
-        },
-        body:JSON.stringify(data)
-      }
-    )
-
-  }catch(error){
-
-    console.error(
-      'Webhook error:',
-      error
-    )
-
-  }
-}
