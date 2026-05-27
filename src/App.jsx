919a2f562e008bc5ecdc6b7a0ae73fd7f6c5317b
Crowsk1ck <crowskick4133@gmail.com>
2026-05-28 01:24:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -115,7 +115,7 @@ export default function App(){
             path="/Members"
             element={
               isAuth
-              ? <Economy />
+              ? <Members />
               : <Dashboard />
             }
           />
