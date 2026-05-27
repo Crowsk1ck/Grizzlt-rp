c73196c2374f28813d5b2c3f977636c7f1f0c064
Crowsk1ck <crowskick4133@gmail.com>
2026-05-27 18:39:07 +0200

    test

--- src/pages/api/discord-members.js
+++ src/pages/api/discord-members.js
@@ -1,8 +1,8 @@
-export default async function handler(req,res){
+export default function handler(req, res) {
 
   res.status(200).json({
-    tokenExists: !!process.env.DISCORD_BOT_TOKEN
-    hello:true
+    tokenExists: !!process.env.DISCORD_BOT_TOKEN,
+    hello: true
   })
 
 }
