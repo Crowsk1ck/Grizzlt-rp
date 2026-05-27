b72cf96241490623c8c0f5dd7099c24c1e965508
Crowsk1ck <crowskick4133@gmail.com>
2026-05-27 18:29:45 +0200

    Update discord-members.js

--- src/pages/api/discord-members.js
+++ src/pages/api/discord-members.js
@@ -1,29 +1,7 @@
 export default async function handler(req,res){
 
-  try{
-
-    const response = await fetch(
-      'https://discord.com/api/v10/guilds/1388989912996380713?with_counts=true',
-      {
-        headers:{
-          Authorization:`Bot ${process.env.DISCORD_BOT_TOKEN}`
-        }
-      }
-    )
-
-    const data = await response.json()
-
-    res.status(200).json({
-      status:response.status,
-      data:data
-    })
-
-  }catch(error){
-
-    res.status(500).json({
-      error:String(error)
-    })
-
-  }
+  res.status(200).json({
+    tokenExists: !!process.env.DISCORD_BOT_TOKEN
+  })
 
 }
