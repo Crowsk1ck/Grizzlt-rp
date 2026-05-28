03ad662fbca7a3e945017aa9c8ab863d3b48121a
Crowsk1ck <crowskick4133@gmail.com>
2026-05-28 16:13:26 +0200

    Update Topbar.jsx

--- src/components/layout/Topbar.jsx
+++ src/components/layout/Topbar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-import '../styles/topbar.css'
+import '../../styles/topbar.css'
 
 const LOGIN_URL =
 'https://discord.com/oauth2/authorize?client_id=1508833507894624399&response_type=token&redirect_uri=https%3A%2F%2Fwww.grizzly-family.online%2F&scope=identify'
